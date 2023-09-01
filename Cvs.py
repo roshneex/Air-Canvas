2c26625c7ffad47d1b4718394d8fff51177efbbc
Roshneex <130371185+roshneex@users.noreply.github.com>
2023-09-01 17:03:32 +0530

    Update Cvs.py

--- Cvs.py
+++ Cvs.py
@@ -1,29 +1,79 @@
 import cv2
+import numpy as np
 import mediapipe as mp
+from collections import deque
 
-mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5)
 
-camra = cv2.VideoCapture("http://192.168.0.198:4747/video")
+# deque for coordinates
+bpoints = [deque(maxlen=1024)]
+gpoints = [deque(maxlen=1024)]
+rpoints = [deque(maxlen=1024)]
+ypoints = [deque(maxlen=1024)]
 
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-while True:
-    ret, frame = camra.read()
-    if not ret:
-        break
+blue_index = 0
+green_index = 0
+red_index = 0
+yellow_index = 0
 
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(frame_rgb)
+# r,g,b,y
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+colorIndex = 0
+
+# whitey canvas dimensions + colour
+paintWindow = np.zeros((471, 636, 3)) + 255
+paintWindow = cv2.rectangle(paintWindow, (40, 1), (140, 65), (0, 0, 0), 2)
+paintWindow = cv2.rectangle(paintWindow, (160, 1), (255, 65), (255, 0, 0), 2)
+paintWindow = cv2.rectangle(paintWindow, (275, 1), (370, 65), (0, 255, 0), 2)
+paintWindow = cv2.rectangle(paintWindow, (390, 1), (485, 65), (0, 0, 255), 2)
+paintWindow = cv2.rectangle(paintWindow, (505, 1), (600, 65), (0, 255, 255), 2)
+
+cv2.putText(paintWindow, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
+
+# cap= cv2.VideoCapture("http://192.168.0.198:4747/video")
+cap = cv2.VideoCapture(0)
+
+ret = True
+while ret:
+    ret, frame = cap.read()
+
+    x, y, c = frame.shape
+
+    # frame flip
+    frame = cv2.flip(frame, 1)
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    frame = cv2.rectangle(frame, (40, 1), (140, 65), (0, 0, 0), 2)
+    frame = cv2.rectangle(frame, (160, 1), (255, 65), (255, 0, 0), 2)
+    frame = cv2.rectangle(frame, (275, 1), (370, 65), (0, 255, 0), 2)
+    frame = cv2.rectangle(frame, (390, 1), (485, 65), (0, 0, 255), 2)
+    frame = cv2.rectangle(frame, (505, 1), (600, 65), (0, 255, 255), 2)
+    cv2.putText(frame, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+
+    results = hands.process(framergb)
 
     if results.multi_hand_landmarks:
         for landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
 
     cv2.imshow("Frame", frame)
+    cv2.imshow("Paint", paintWindow)
 
     if cv2.waitKey(1) == ord("q"):
         break
 
-camra.release()
+cap.release()
 cv2.destroyAllWindows()
