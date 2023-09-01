7af89200092a0c61cc4db4eac8084f93a615e62b
Roshneex <130371185+roshneex@users.noreply.github.com>
2023-09-02 02:22:24 +0530

    Update Cvs.py

--- Cvs.py
+++ Cvs.py
@@ -3,17 +3,18 @@ import numpy as np
 import mediapipe as mp
 from collections import deque
 
+# mediapipe
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5)
 
-# deque for coordinates
+# deque for coordinate thingy
 bpoints = [deque(maxlen=1024)]
 gpoints = [deque(maxlen=1024)]
 rpoints = [deque(maxlen=1024)]
 ypoints = [deque(maxlen=1024)]
 
-
+# one colour at a time
 blue_index = 0
 green_index = 0
 red_index = 0
@@ -43,11 +44,12 @@ cap = cv2.VideoCapture(0)
 
 ret = True
 while ret:
+    # Read each frame from the webcam
     ret, frame = cap.read()
 
     x, y, c = frame.shape
 
-    # frame flip
+    # Flip the frame vertically
     frame = cv2.flip(frame, 1)
     # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -63,17 +65,99 @@ while ret:
     cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
     cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
+    # Get hand landmark prediction
     results = hands.process(framergb)
 
     if results.multi_hand_landmarks:
-        for landmarks in results.multi_hand_landmarks:
+        landmarks = []
+        for handslms in results.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                # # print(id, lm)
+                # print(lm.x)
+                # print(lm.y)
+                lmx = int(lm.x * 640)
+                lmy = int(lm.y * 480)
+
+                landmarks.append([lmx, lmy])
+
             mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+            fore_finger = (landmarks[8][0], landmarks[8][1])
+            center = fore_finger
+            thumb = (landmarks[4][0], landmarks[4][1])
+            cv2.circle(frame, center, 3, (0, 255, 0), -1)
+            print(center[1] - thumb[1])
+            if (thumb[1] - center[1] < 30):
+                bpoints.append(deque(maxlen=512))
+                blue_index += 1
+                gpoints.append(deque(maxlen=512))
+                green_index += 1
+                rpoints.append(deque(maxlen=512))
+                red_index += 1
+                ypoints.append(deque(maxlen=512))
+                yellow_index += 1
+
+            elif center[1] <= 65:
+                if 40 <= center[0] <= 140:  # Clear Button
+                    bpoints = [deque(maxlen=512)]
+                    gpoints = [deque(maxlen=512)]
+                    rpoints = [deque(maxlen=512)]
+                    ypoints = [deque(maxlen=512)]
+
+                    blue_index = 0
+                    green_index = 0
+                    red_index = 0
+                    yellow_index = 0
+
+                    paintWindow[67:, :, :] = 255
+                elif 160 <= center[0] <= 255:
+                    colorIndex = 0  # Blue
+                elif 275 <= center[0] <= 370:
+                    colorIndex = 1  # Green
+                elif 390 <= center[0] <= 485:
+                    colorIndex = 2  # Red
+                elif 505 <= center[0] <= 600:
+                    colorIndex = 3  # Yellow
+            else:
+                if colorIndex == 0:
+                    bpoints[blue_index].appendleft(center)
+                elif colorIndex == 1:
+                    gpoints[green_index].appendleft(center)
+                elif colorIndex == 2:
+                    rpoints[red_index].appendleft(center)
+                elif colorIndex == 3:
+                    ypoints[yellow_index].appendleft(center)
+            # next deques
+        else:
+            bpoints.append(deque(maxlen=512))
+            blue_index += 1
+            gpoints.append(deque(maxlen=512))
+            green_index += 1
+            rpoints.append(deque(maxlen=512))
+            red_index += 1
+            ypoints.append(deque(maxlen=512))
+            yellow_index += 1
+
+            # lines of all the colors
+        points = [bpoints, gpoints, rpoints, ypoints]
+        # for j in range(len(points[0])):
+        #         for k in range(1, len(points[0][j])):
+        #             if points[0][j][k - 1] is None or points[0][j][k] is None:
+        #                 continue
+        #             cv2.line(paintWindow, points[0][j][k - 1], points[0][j][k], colors[0], 2)
+        for i in range(len(points)):
+            for j in range(len(points[i])):
+                for k in range(1, len(points[i][j])):
+                    if points[i][j][k - 1] is None or points[i][j][k] is None:
+                        continue
+                    cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+                    cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Paint", paintWindow)
 
     if cv2.waitKey(1) == ord("q"):
         break
+
 
 cap.release()
 cv2.destroyAllWindows()
